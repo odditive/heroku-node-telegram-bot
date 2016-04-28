8363be5a156af7eb85dc342a941920f12e4ec084
Volodymyr Lut <volodymyrgalan@gmail.com>
2016-04-28 13:24:40 +0300

    Update listening port from localhost to 0.0.0.0

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, "localhost", function () {
+var server = app.listen(process.env.PORT, "0.0.0.0", function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Web server started at http://%s:%s', host, port);
